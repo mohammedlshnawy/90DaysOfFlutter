284b00d321469d0d9ce32b7a5ff7cfa0acf4d54b
Mohammed Shnawy <mohammedlshnawy@gmail.com>
2026-03-22 06:02:42 +0200

    small progress update

--- PhaseOne/WordFreqCounter/bin/wfcli.dart
+++ PhaseOne/WordFreqCounter/bin/wfcli.dart
@@ -1,31 +1,43 @@
+import 'dart:io';
+
 const version = '0.0.1';
+
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == "help") {
+  final inputFile = arguments.length > 1
+      ? arguments.sublist(2).join(' ')
+      : null;
+  final inputCount = arguments.length > 1 ? arguments[1] : null;
+  int cnt = int.tryParse(inputCount ?? "") ?? 0;
+
+  if (arguments.isEmpty || arguments.first == "--help") {
     // default/help prompt
-    usage();
-  } else if (arguments.first == "version") {
+    help();
+  } else if (arguments.first == "--v") {
     // cli version checker
     print(
       "WFC Cli Version: $version (Under Development)",
     ); // output: WFC Cli Version: 0.0.1 (Under Development)
-  } else if (arguments.first == "most") {
-    final inputFile = arguments.length > 1 ? arguments.sublist(1) : null;
-    mostFreq(inputFile);
+  } else if (arguments.first == "--top") {
+    top(inputFile.toString(), cnt);
   } else {
-    usage();
+    help();
   }
 }
 
-void usage() {
+void help() {
   print(
     "\nThe following commands are valid:"
     "\n\nhelp: help command that guid u"
     ",\nversion: Cli Version Checker"
-    ",\nmost <FILE>: Print the most frequent words on a file "
+    ",\n--top <FILE>: Print the top 10 frequent words on a file "
     "\n\nThese Features Still Under Development\n",
   );
 }
 
-dynamic mostFreq(List<String>? file) {
-  print('still working on it');
+dynamic top(String file, int count) async {
+  await File(file).readAsString().then((String content) {
+    var str = content.toLowerCase().replaceAll(RegExp(r'[^\w\s]'), '').split(RegExp(r'\s+'));
+    print(str);
+    print(count);
+  });
 }
